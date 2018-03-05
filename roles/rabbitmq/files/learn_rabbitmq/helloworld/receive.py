c403901dc24151d3ec2339e084f7f8aa32e44e36
Rifki Chaplin <rifkichaplin@gmail.com>
2018-03-06 00:02:07 +0700

    Add code credential in consumer rabbitMQ

--- roles/rabbitmq/files/learn_rabbitmq/helloworld/receive.py
+++ roles/rabbitmq/files/learn_rabbitmq/helloworld/receive.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+credentials = pika.credentials.PlainCredentials('ksl-ubl', 'ksl-ubl', erase_on_connect=False)
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1', credentials=credentials, socket_timeout=2))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
